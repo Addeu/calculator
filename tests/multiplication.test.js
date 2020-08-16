47e0abb6062d43a361feb45f45e92fa74c6d1579
Danil 'Addeu' Deulin <addeulin@gmail.com>
2020-08-16 12:57:11 +0500

    Multiplication tests

--- tests/multiplication.test.js
+++ tests/multiplication.test.js
@@ -1,5 +1,37 @@
-import {describe} from "mocha";
+const mocha = require('mocha');
+const chai = require('chai');
+const Calculator = require('../bin/calculator');
+const expect = chai.expect;
+const calc = new Calculator();
 
-describe('', function() {
+describe('Multiplication tests', function() {
+	it('Checks if multiplication of two integers is correct', () => {
+		let intExpression = calc.calculate('2 * 2');
+		expect(intExpression).to.equal(4);
+	});
 
+	it('Checks if multiplication of two floats is correct', () => {
+		let intExpression = calc.calculate('0.2 * 0.3');
+		expect(intExpression).to.equal(0.06);
+	});
+
+	it('Checks if multiplication of multiple integers is correct', () => {
+		let intExpression = calc.calculate('2 * 2 * 3 * 5');
+		expect(intExpression).to.equal(60);
+	});
+
+	it('Checks if multiplication of multiple floats is correct', () => {
+		let intExpression = calc.calculate('1.1 * 0.9 * 1.2 * 1.0');
+		expect(intExpression).to.equal(1.1880000000000002);
+	});
+
+	it('Checks if multiplication of integer and float is correct', () => {
+		let intExpression = calc.calculate('4 * 0.9');
+		expect(intExpression).to.equal(3.6);
+	});
+
+	it('Checks if multiplication of multiple integers and floats is correct', () => {
+		let intExpression = calc.calculate('555 * 60 * 0.5 * 35 * 8.15');
+		expect(intExpression).to.equal(4749412.5);
+	});
 });
