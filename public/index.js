0ee8576d4a68b06a2ae48e5e635ef1e7e9980969
Danil 'Addeu' Deulin <addeulin@gmail.com>
2020-08-20 09:27:34 +0500

    Front end update

---
+++ public/index.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let inputButtons = document.getElementsByClassName('input');
+	let clearBtn = document.getElementById('clear');
+	let equalsBtn = document.getElementById('equals');
+
+	for (let i = 0; i < inputButtons.length; ++i) {
+		inputButtons[i].addEventListener('click', () => {
+			input(inputButtons[i].innerHTML);
+		});
+	}
+
+	clearBtn.addEventListener('click', clear);
+	equalsBtn.addEventListener('click', calculate);
+});
+
+function input(value) {
+	document.getElementById('inputField').innerHTML += value;
+}
+
+function clear() {
+	document.getElementById('inputField').innerHTML = '';
+}
+
+async function calculate() {
+	let input = document.getElementById('inputField');
+	let data = {expression: input.innerHTML};
+
+	let response = await fetch('/api/calculate', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	});
+
+	if (response.ok) {
+		let result = await response.text();
+		input.innerHTML = result;
+	} else {
+		input.innerHTML = 'ERROR';
+	}
+}
