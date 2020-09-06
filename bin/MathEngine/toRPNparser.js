40db97e30a7561973fcde25c4380ab49948bffab
Danil 'Addeu' Deulin <addeulin@gmail.com>
2020-09-06 13:21:49 +0500

    Reversed Polish Notation parser

---
+++ bin/MathEngine/toRPNparser.js
@@ -0,0 +1,57 @@
+const Stack = require('../Utils/stack');
+
+/**
+ * @class {ToRPNParser}
+ */
+module.exports = class ToRPNParser {
+	/**
+	 * @public
+	 * @param {Array.<Token>} tokenSet
+	 * @returns {Stack} consider as Array
+	 */
+	postfixNotation(tokenSet) {
+
+		let output = new Stack();
+		let operators = new Stack();
+
+		for (let i = 0; i<tokenSet.length; i++) {
+
+			let currentToken = tokenSet[i];
+
+			if (currentToken.type === 'number') {
+				output.push(currentToken);
+			}
+			
+			if (currentToken.type === 'operator') {
+				while (
+				(!operators.isEmpty() && operators.last().type !== 'parenthesis') &&
+				((operators.last().precedence >= currentToken.precedence && currentToken.associativity === 'left') ||
+					(operators.last().precedence > currentToken.precedence && currentToken.associativity === 'right'))
+					) {
+					output.push(operators.pop());
+				}
+				operators.push(currentToken);
+			}
+			
+			if (currentToken.type === 'parenthesis') {
+				if (currentToken.value() === '(') {
+					operators.push(currentToken);
+				}
+				
+				if (currentToken.value() === ')') {
+					
+					while (!operators.isEmpty() && operators.last().value() !== '(') {
+						output.push(operators.pop());
+					}
+					
+					operators.pop();
+				}
+
+			}
+		}
+		while (!operators.isEmpty()) {
+			output.push(operators.pop());
+		}
+		return output;
+	}	
+};
