ce1d92ce1f718bf775342d9c6946786bb169e817
Danil 'Addeu' Deulin <addeulin@gmail.com>
2020-09-06 12:09:33 +0500

    token class

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const Calculator = require('../bin/calculator');
 const Validator = require('../bin/inputValidator');
+const MathEngine = require('../bin/MathEngine/mathEngine');
 
 router.post('/calculate', (req, res) =>{
 	let expression = req.body.expression;
-	let calculator = new Calculator(new Validator());
+	let calculator = new Calculator(new Validator(), new MathEngine());
 
 	res.json(calculator.calculate(expression));
 });
