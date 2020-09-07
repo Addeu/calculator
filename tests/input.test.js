2842894398f24547d57cd220c11f31cf8df93897
Danil 'Addeu' Deulin <addeulin@gmail.com>
2020-09-07 21:51:02 +0500

    input tests reformat

--- tests/input.test.js
+++ tests/input.test.js
@@ -8,41 +8,41 @@ const validator = new Validator(CONFIG.prohibitedChars, CONFIG.legalOperations);
 describe('Input tests', function() {
 	it('Checks if returns input if no operations were included', () => {
 		let expression = validator.hasNoOperations('256');
-		expect(expression).to.equal(true);
+		expect(expression).to.be.true;
 	});
 
 	it('Checks if returns input if no operations were included and negatives were used', () => {
 		let expression = validator.hasNoOperations('-25');
-		expect(expression).to.equal(true);
+		expect(expression).to.be.true;
 	});
 
 	it('Checks if rejects mixed digit-letter strings', () => {
 		let expression = validator.isInputValid('2absfdh4');
-		expect(expression).to.equal(false);
+		expect(expression).to.be.false;
 	});
 
 	it('Checks if rejects unsupported chars', () => {
 		let expression = validator.isInputValid('21&2!2');
-		expect(expression).to.equal(false);
+		expect(expression).to.be.false;
 	});
 
 	it('Checks if rejects wrong parenthesis chars', () => {
 		let expression = validator.isInputValid('2 * {1}');
-		expect(expression).to.equal(false);
+		expect(expression).to.be.false;
 	});
 
 	it('Checks if rejects wrong parenthesis placement', () => {
 		let expression = validator.isInputValid('2 * (2 - (3)');
-		expect(expression).to.equal(false);
+		expect(expression).to.be.false;
 	});
 
 	it('Checks if rejects double operators', () => {
 		let expression = validator.isInputValid('2 ++ 2');
-		expect(expression).to.equal(false);
+		expect(expression).to.be.false;
 	});
 
 	it('Checks if accepts correct parenthesis placement', () => {
 		let expression = validator.isInputValid('2 * (3 + 1)');
-		expect(expression).to.equal(true);
+		expect(expression).to.be.true;
 	});
 });
